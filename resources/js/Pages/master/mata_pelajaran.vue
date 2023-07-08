9ea34c3984686448c912c81e25635a37b897f713
Joshua Sitompul <joshuasitompul2000@gmail.com>
2023-07-08 14:33:24 +0700

    + view master

---
+++ resources/js/Pages/master/mata_pelajaran.vue
@@ -0,0 +1,18 @@
+<script>
+import menu_layout from '../../layout/menu_layout.vue';
+import {useStore} from "vuex"
+export default {
+    layout : menu_layout,
+    setup(){
+        const store = useStore()
+        store.state.page.bagian = "Master"
+        store.state.page.judul = "Mata Pelajaran"
+    }
+}
+</script>
+
+<template>
+    <div>
+        Dashboard
+    </div>
+</template>
