b0780b4027cd1a089420e48a6afca6ba367af1d9
Joshua Sitompul <joshuasitompul2000@gmail.com>
2023-07-16 17:02:43 +0700

    tamabh jurusan

--- app/Http/Controllers/jurusan/JurusanController.php
+++ app/Http/Controllers/jurusan/JurusanController.php
@@ -3,6 +3,8 @@
 namespace App\Http\Controllers\jurusan;
 
 use App\Http\Controllers\Controller;
+use App\Http\Requests\jurusan\JurusanStoreRequest;
+use App\Models\Jurusan;
 use Illuminate\Http\Request;
 
 class JurusanController extends Controller
@@ -12,9 +14,24 @@ class JurusanController extends Controller
      *
      * @return \Illuminate\Http\Response
      */
-    public function index()
+    public function index(Request $request)
     {
-        return inertia()->render('master/jurusan');
+        $params = [
+            'cari' => !empty($request->cari) ? $request->cari : '',
+            'show' => !empty($request->show) ? $request->show : 5,
+        ];
+
+        $kelas = Jurusan::where(function($q) use($params){
+            $q->where("jurusan","like","%{$params['cari']}%");
+        });
+        $kelas = $kelas->latest();
+        $kelas = $kelas->paginate($params['show'])->withQueryString();
+
+        $data = [
+            "jurusan" => $kelas,
+            'params' => $params,
+        ];
+        return inertia()->render('master/jurusan',$data);
     }
 
     /**
@@ -33,9 +50,11 @@ class JurusanController extends Controller
      * @param  \Illuminate\Http\Request  $request
      * @return \Illuminate\Http\Response
      */
-    public function store(Request $request)
+    public function store(JurusanStoreRequest $request)
     {
-        //
+        $jurusan = new Jurusan();
+        $jurusan->jurusan = $request->jurusan;
+        $jurusan->save();
     }
 
     /**
--- resources/js/Pages/master/jurusan.vue
+++ resources/js/Pages/master/jurusan.vue
@@ -1,18 +1,215 @@
 <script>
-import menu_layout from '../../layout/menu_layout.vue';
-import {useStore} from "vuex"
+import { router, useForm } from "@inertiajs/vue3";
+import menu_layout from "../../layout/menu_layout.vue";
+import modal_normal from "../../component/modal/modal_normal.vue"
+import input_normal from "../../component/input/input_normal.vue"
+import { useStore } from "vuex";
 export default {
-    layout : menu_layout,
-    setup(){
-        const store = useStore()
-        store.state.page.bagian = "Master"
-        store.state.page.judul = "Jurusan"
-    }
-}
+    layout: menu_layout,
+    props: ["jurusan", "params"],
+    components : {
+        modal_normal,
+        input_normal
+    },
+    setup() {
+        const store = useStore();
+        store.state.page.bagian = "Master";
+        store.state.page.judul = "Jurusan";
+
+        const form_tambah = useForm({
+            jurusan : "",
+        });
+
+        const form_edit = useForm({
+            id : null,
+            jurusan : "",
+        });
+
+        const form_hapus = useForm({
+            id : null,
+            jurusan : "",
+        });
+
+        return {
+            form_tambah,
+            form_edit,
+            form_hapus,
+        }
+    },
+    data() {
+        return {
+            show: this.params.show,
+            cari: this.params.cari,
+        };
+    },
+    methods: {
+        get_data() {
+            const params = {
+                show: this.show,
+                cari: this.cari,
+            };
+            router.get(route("master.jurusan.index"), params, {
+                preserveState: true,
+            });
+        },
+        submit_tambah() {
+            this.form_tambah.post(route("master.jurusan.store"), {
+                onSuccess: () => {
+                    document.getElementById("tambah")?.click();
+                    this.form_tambah.reset();
+                },
+            });
+        },
+        data_edit(data) {
+            this.form_edit.id = data.id;
+            this.form_edit.jurusan = data.jurusan;
+        },
+        submit_edit() {
+            this.form_edit.put(
+                route("master.jurusan.update", { id: this.form_edit.id }),
+                {
+                    onSuccess: () => {
+                        document.getElementById("edit")?.click();
+                        this.form_edit.reset();
+                    },
+                }
+            );
+        },
+    },
+    watch: {
+        show() {
+            this.get_data();
+        },
+        cari() {
+            this.get_data();
+        },
+    },
+};
 </script>
 
 <template>
-    <div>
-        Dashboard
+    <div class="flex flex-col gap-2">
+        <div class="flex justify-between">
+            <label for="tambah" class="btn btn-xs bg-success">
+                <i class="fa fa-plus"></i> tambah
+            </label>
+            <div class="flex gap-4">
+                <div class="form-control">
+                    <input
+                        type="text"
+                        class="input input-xs input-bordered"
+                        placeholder="Search..."
+                        v-model="cari"
+                    />
+                </div>
+                <div class="flex gap-2 justify-center items-center">
+                    <div class="text-sm">Tampilkan</div>
+                    <select
+                        class="select select-bordered select-xs"
+                        v-model="show"
+                    >
+                        <option :value="5">5 baris</option>
+                        <option :value="10">10 baris</option>
+                        <option :value="20">20 baris</option>
+                    </select>
+                </div>
+            </div>
+        </div>
+
+        <!-- tabel  -->
+        <div class="overflow-x-auto scrollbar-hide">
+            <table class="table table-xs table-zebra">
+                <thead>
+                    <tr class="bg-sky-200">
+                        <th>NO.</th>
+                        <th>Jurusan</th>
+                        <th class="text-center">Opsi</th>
+                    </tr>
+                </thead>
+                <tbody>
+                    <transition-group name="table">
+                        <tr
+                            v-for="(item, index) in jurusan?.data"
+                            :key="item?.id"
+                            v-if="jurusan?.data?.length"
+                        >
+                            <td>{{ index + 1 }}</td>
+                            <td class="uppercase">{{ item.jurusan }}</td>
+                            <td class="w-40">
+                                <div class="flex gap-2 w-full">
+                                    <label
+                                        @click="data_edit(item)"
+                                        for="edit"
+                                        class="btn btn-xs bg-warning"
+                                    >
+                                        <i class="fa fa-pen"></i> edit
+                                    </label>
+                                    <label
+                                        @click="data_hapus(item)"
+                                        for="hapus"
+                                        class="btn btn-xs bg-error"
+                                    >
+                                        <i class="fa fa-trash"></i> hapus
+                                    </label>
+                                </div>
+                            </td>
+                        </tr>
+                        <tr v-else>
+                            <td colspan="10" class="text-center font-bold">
+                                Belum ada data!
+                            </td>
+                        </tr>
+                    </transition-group>
+                </tbody>
+            </table>
+        </div>
+
+        <!-- pagination  -->
+        <div
+            class="flex justify-center items-center"
+            v-if="jurusan.data.length"
+        >
+            <div class="join">
+                <Link
+                    as="button"
+                    class="join-item btn btn-xs"
+                    :class="{ 'bg-active': item.active }"
+                    v-for="(item, index) in jurusan.links"
+                    :key="index"
+                    v-html="item.label"
+                    :disabled="!item.url"
+                    :href="item.url ? item.url : '#'"
+                    :preserve-state="true"
+                ></Link>
+            </div>
+        </div>
     </div>
+
+    <!-- tambah  -->
+    <modal_normal id="tambah" title="Tambah Jurusan">
+        <input_normal title="Jurusan" v-model="form_tambah.jurusan" :error="form_tambah.errors.jurusan" placeholder="Masukkan Jurusan" :length="50"/>
+        <template v-slot:action>
+            <button
+                class="btn bg-success"
+                :class="{ 'loading btn-disabled': form_tambah.processing }"
+                @click="submit_tambah"
+            >
+                simpan
+            </button>
+        </template>
+    </modal_normal>
+
+    <!-- edit  -->
+    <modal_normal id="edit" title="Edit Jurusan">
+        <input_normal title="Jurusan" v-model="form_edit.jurusan" :error="form_edit.errors.jurusan" placeholder="Masukkan Jurusan" :length="50"/>
+        <template v-slot:action>
+            <button
+                class="btn bg-warning"
+                :class="{ 'loading btn-disabled': form_edit.processing }"
+                @click="submit_edit"
+            >
+                edit
+            </button>
+        </template>
+    </modal_normal>
 </template>
