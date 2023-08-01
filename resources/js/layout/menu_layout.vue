d27064d1b7b53ae8d08160a0804966c06c8283f8
Joshua Sitompul <joshuasitompul2000@gmail.com>
2023-08-01 11:26:28 +0700

    ubah tampilan menu

--- resources/js/layout/menu_layout.vue
+++ resources/js/layout/menu_layout.vue
@@ -93,7 +93,7 @@ export default {
                         tabindex="0"
                         class="mt-3 z-[1] p-2 shadow menu menu-sm dropdown-content bg-base-100 rounded-box w-52"
                     >
-                        <li>
+                        <li v-if="else">
                             <a class="justify-between">
                                 Profile
                                 <span class="badge">New</span>
